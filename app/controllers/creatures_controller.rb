b44c528de9dacfe6585913426a63fa7bc245a1a6
Allxie <allxiecleary@gmail.com>
2015-04-27 14:37:41 -0700

    working crud

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -12,8 +12,40 @@ class CreaturesController < ApplicationController
 	
     def create
         new_creature = params.require(:creature).permit(:name, :description)
-        creature.create(new_creature)
-        redirect_to "/creatures"
+        creature = Creature.create(new_creature)
+        redirect_to "/creatures/#{creature.id}"
+    end
+
+	def show
+        id = params[:id]
+        @creature = Creature.find(id)
+        render :show
+    end
+
+    def edit
+        id = params[:id]
+        @creature = Creature.find(id)
+        render :edit
+    end
+
+    def update
+        creature_id = params[:id]
+        creature = Creature.find(creature_id)
+
+        # get updated data
+        updated_attributes = params.require(:creature).permit(:name, :description)
+        # update the creature
+        creature.update_attributes(updated_attributes)
+
+        #redirect to show
+        redirect_to "/creatures/#{creature_id}"
+    end
+
+	def destroy
+	    id = params[:id]
+	    creature = Creature.find(id)
+	    creature.destroy
+	    redirect_to "/creatures"
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
   root to: 'creautures#index'
   ## Also just to keep it RESTful
-  get '/creatures', to: "creatures#index"
-
+  get '/creatures', to: "creatures#index", as: "creatures"
   
-  get '/creatures/new', to: 'creatures#new'
+  get '/creatures/new', to: 'creatures#new', as: "new_creatures"
 
+  # rake routes to check this route out
+  get '/creatures/:id', to: 'creatures#show', as: "creature"
+
+  get '/creatures/:id/edit', to: 'creatures#edit', as: "edit_creature"
 
   post "/creatures", to: "creatures#create"
-  
+
+  # The update route
+  patch "/creatures/:id", to: "creatures#update" 
+
+  # the destroy route
+  delete "/creatures/:id", to: "creatures#destroy"
+
 end
